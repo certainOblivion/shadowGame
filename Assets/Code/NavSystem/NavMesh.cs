0704be05827baccf20ef4d207c2c0176dfe4c351
prithiraj2005 <prithiraj2005@gmail.com>
2016-07-01 03:01:49 -0400

    forgot to add a file from last sync

--- Assets/Code/NavSystem/NavMesh.cs
+++ Assets/Code/NavSystem/NavMesh.cs
@@ -7,57 +7,39 @@ public class NavMesh : MonoBehaviour {
 
     public float HexScale;
     public float WorldToGridRatio;
-    public bool EnableDebugDraw;
-    private RectangleMap mNavMap;
     private MeshRenderer mFloorMesh;
     private List<NavMeshObstacle> mNavObstacles;
     private List<NavMeshActor> mNavMeshActors;
+    NavMeshPath mNavMesh;
+    List<Hex> path;
     // Use this for initialization
     void Start () {
 
         mFloorMesh = GetComponent<MeshRenderer>();
-
-        mNavMap = new RectangleMap((int)mFloorMesh.bounds.size.y * HexScale , (int)mFloorMesh.bounds.size.x * HexScale, HexScale, WorldToGridRatio);
-
         mNavObstacles = new List<NavMeshObstacle>(FindObjectsOfType<NavMeshObstacle>());
         mNavMeshActors = new List<NavMeshActor>(FindObjectsOfType<NavMeshActor>());
+        mNavMesh = new NavMeshPath(mFloorMesh.bounds.size.x * HexScale, mFloorMesh.bounds.size.y * HexScale, HexScale, WorldToGridRatio, mNavObstacles, mNavMeshActors);
+        path = new List<Hex>();
     }
 
     // Update is called once per frame
-    void Update () {
-
-        if (EnableDebugDraw)
+    void Update ()
+    {
+        if (mNavMeshActors.Count == 0)
         {
-            float quadHeight = Camera.main.orthographicSize * 2.0f;
-            float quadWidth = quadHeight * Screen.width / Screen.height;
-
-            DebugDrawHelper.DrawRectangle(mFloorMesh.transform.position, quadWidth, quadHeight, color:Color.red);
-
-            foreach (Hex hex in mNavMap.Map)
-            {
-                GridHelper.DrawHex(mNavMap.MapLayout, hex, Color.yellow);
-            }
-            foreach (NavMeshObstacle obstacle in mNavObstacles)
-            {
-                Vector2 obstacleCenter = new Vector2(obstacle.gameObject.transform.position.x, obstacle.gameObject.transform.position.y);
-                Vector3 obstacleVolume = obstacle.gameObject.GetComponent<Renderer>().bounds.size;
-                Vector2 obstacleVolume2D = new Vector2(obstacleVolume.x, obstacleVolume.y);
-                foreach (Hex hex in GridHelper.BoxToHexList(obstacleCenter, obstacleVolume2D, mNavMap))
-                {
-                    GridHelper.DrawHex(mNavMap.MapLayout, hex, Color.blue, true);
-                }
-            }
-            foreach (NavMeshActor actor in mNavMeshActors)
-            {
-                Point position = GridHelper.Vector3ToPoint(actor.transform.position);
-                
-                Hex occupiedHex = FractionalHex.HexRound(Layout.PixelToHex(mNavMap.MapLayout, position));
-
-                GridHelper.DrawHex(mNavMap.MapLayout, occupiedHex, Color.green, true);
-            }
+            return;
+        }
+        if (Input.GetMouseButton(0))
+        {
+            path = mNavMesh.GetPath(mNavMeshActors[0].transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            mNavMeshActors[0].SetPath(path);          
         }
 
-        
+        foreach (Hex hex in path)
+        {
+            GridHelper.DrawHex(mNavMesh.NavMap.MapLayout, hex, Color.red);
+        }
+
     }
 
 
